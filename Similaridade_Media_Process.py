e254085d3e8f803be5d7edcb56f1fdfca2b6e890
VictorRivelo <victor.rivelo@usp.br>
2018-06-23 18:15:56 -0300

    distancia entre coordenadas em segundos

--- Similaridade_Media_Process.py
+++ Similaridade_Media_Process.py
@@ -1,12 +1,26 @@
-##Converter as coordenadas em um local e 
+##Converter as coordenadas em um local em um ponto
 def gpsdData(dataLatitude, dataLongitude):
 
     return 
 
 ##calcular a dist entre dois pontos
-def gpsdData(data1, data2):
+##latitude e longitude utilizadas em segundos
 
-    return 
+import math
+public double distancia (int latitudeOrigem, int longitudeOrigem, int latitudeDestino, int longitudeDestino){
+
+      double circTerra=40030; // Circunferência da Terra (em kilômetros)
+      double latitudeO = (double) latitudeOrigem / 3600;
+      double longitudeO = (double) longitudeOrigem / 3600;
+      double latitudeD = (double) latitudeDestino / 3600;
+      double longitudeD = (double) longitudeDestino / 3600;
+      double a = longitudeO - longitudeD;
+      double c = 90.0 - latitudeO;
+      double b = 90.0 - latitudeD;       
+      double aCosA =  (Math.acos(Math.cos(radians(b)) * Math.cos(radians(c)) + Math.sin(radians(b)) * Math.sin(radians(c)) * Math.cos(radians(a))) * 180 / Math.PI);
+
+      return(aCosA * circTerra / 360);
+} 
 
 ##Calcula a dist média para cada motorista 
 
