e99cf5b17f8589da74354ef4132d5f72fc58a4f0
guilherme <guilherme.milan9@gmail.com>
2018-06-23 11:07:19 -0300

    remocao de codigos comentados na discretizacao

--- discretiza_trajetorias.py
+++ discretiza_trajetorias.py
@@ -3,17 +3,14 @@ import pandas as pd
 def discretiza_trajetorias(grid_size):
 
 	data = pd.read_csv('DADOS_TREINAMENTO.csv')
-	#split_data = pd.DataFrame().row.str.split()
+	
 	data['POLYLINE'] = data['POLYLINE'].str[1:]
 	data['POLYLINE'] = data['POLYLINE'].str.replace('], ','];')
-	
 	d2 = data['POLYLINE'].str.split(';',expand=True)
 	data = data.drop('POLYLINE', axis=1)
 	data = data.join(d2)
 
-	#data['POLYLINE'] = data['POLYLINE'].apply(lambda x: pd.DataFrame(x.split(';')))
 	print(data)
-	#print(d2)
 	data.to_csv('dados_preprocessados.csv')
 
 	return 0
