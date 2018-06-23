e0a1622900d806a6a07c6a7bff275ad01a939535
guilherme <guilherme.milan9@gmail.com>
2018-06-23 07:49:11 -0300

    quebra da coluna polyline

--- discretiza_trajetorias.py
+++ discretiza_trajetorias.py
@@ -1,9 +1,19 @@
 import pandas as pd
 
-def discretiza_trajetorias():
+def discretiza_trajetorias(grid_size):
 
-	data = pd.read_csv('data.csv', usecols=['POLYLINE'])
+	data = pd.read_csv('DADOS_TREINAMENTO.csv')
+	#split_data = pd.DataFrame().row.str.split()
+	data['POLYLINE'] = data['POLYLINE'].str[1:]
+	data['POLYLINE'] = data['POLYLINE'].str.replace('], ','];')
+	
+	data_split = data['POLYLINE'].apply(lambda x: pd.DataFrame(x.split(';')))
+
+	print(data_split)
+	data_split.to_csv('dados_preprocessados.csv')
 
 	return 0
 
-discretiza_trajetorias()
+
+
+discretiza_trajetorias(0)
