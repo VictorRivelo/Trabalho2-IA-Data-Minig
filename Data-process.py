0b145e23a1b073a52d85038d69f8188c89b18a44
VictorRivelo <victor.rivelo@usp.br>
2018-06-23 15:00:36 -0300

    divisão de tarefas em arquivos

---
+++ Data-process.py
@@ -0,0 +1,12 @@
+###● Limpeza dos dados – ​Em nossa base de dados existe um atributo DAYTYPE: (char),
+#que identifica a data da corrida informado pelo challenge que possui alguns erros de
+#cálculo podendo influenciar no resultado, uma estratégia é validar os dados
+#comparando com a base de feriados de portugal
+#https://holidays.retira.eu/public-holidays/portugal/
+
+
+##classificar o dia da semana/feriado para um determinada data, montar uma matriz de boolean por exemplo de Dom - seg,  
+def Data(DAYTYPE):
+ 
+
+    return 
---
+++ Similaridade_Media_Process.py
@@ -0,0 +1,15 @@
+##Converter as coordenadas em um local e 
+def gpsdData(dataLatitude, dataLongitude):
+
+    return 
+
+##calcular a dist entre dois pontos
+def gpsdData(data1, data2):
+
+    return 
+
+##Calcula a dist média para cada motorista 
+
+def distMedia():
+
+    return 
