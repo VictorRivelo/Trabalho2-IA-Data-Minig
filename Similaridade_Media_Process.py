c7f994034d49f79831489c5314f3f490c311c1a0
VictorRivelo <VRivelo62@gmail.com>
2018-06-23 20:27:54 -0300

    2 possibilidades de distância

--- Similaridade_Media_Process.py
+++ Similaridade_Media_Process.py
@@ -6,21 +6,36 @@ def gpsdData(dataLatitude, dataLongitude):
 ##calcular a dist entre dois pontos
 ##latitude e longitude utilizadas em segundos
 
+## teste1, erro de 0,5% aproximadamente
 import math
-public double distancia (int latitudeOrigem, int longitudeOrigem, int latitudeDestino, int longitudeDestino){
 
-      double circTerra=40030; // Circunferência da Terra (em kilômetros)
-      double latitudeO = (double) latitudeOrigem / 3600;
-      double longitudeO = (double) longitudeOrigem / 3600;
-      double latitudeD = (double) latitudeDestino / 3600;
-      double longitudeD = (double) longitudeDestino / 3600;
-      double a = longitudeO - longitudeD;
-      double c = 90.0 - latitudeO;
-      double b = 90.0 - latitudeD;       
-      double aCosA =  (Math.acos(Math.cos(radians(b)) * Math.cos(radians(c)) + Math.sin(radians(b)) * Math.sin(radians(c)) * Math.cos(radians(a))) * 180 / Math.PI);
+def distance(originlatitude, originlongitude, destLatitude, destLongitude):
+    lat1 = originlatitude
+    lon1 = originlongitude
+    lat2 = destLatitude
+    lon2 = destLongitude
+    radius = 6371 # km
 
-      return(aCosA * circTerra / 360);
+    dlat = math.radians(lat2-lat1)
+    dlon = math.radians(lon2-lon1)
+    a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(math.radians(lat1)) \
+        * math.cos(math.radians(lat2)) * math.sin(dlon/2) * math.sin(dlon/2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    d = radius * c
+
+    return d
 } 
+
+
+##teste2
+import geopy.distance
+
+def distance(originlatitude, originlongitude, destLatitude, destLongitude):
+
+coords_1 = (originlatitude, originlongitude)
+coords_2 = (destLatitude, destLongitude)
+
+print geopy.distance.vincenty(coords_1, coords_2).km
 
 ##Calcula a dist média para cada motorista 
 
