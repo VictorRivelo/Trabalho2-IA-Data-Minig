bb4a5a0fd109c5a6623722a6322ff4bfefbe6ad2
guilherme <guilherme.milan9@gmail.com>
2018-06-23 06:28:43 -0300

    dataset de treinamento, script de criacao do venv, arquivo que contera o codigo de discretizacao

---
+++ discretiza_trajetorias.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def discretiza_trajetorias():
+
+	data = pd.read_csv('data.csv', usecols=['POLYLINE'])
+
+	return 0
+
+discretiza_trajetorias()
---
+++ venv_create.sh
@@ -0,0 +1,5 @@
+sudo apt-get install python3-tk
+python3 -m venv trabalho2_env --without-pip
+source trabalho2_env/bin/activate
+curl https://bootstrap.pypa.io/get-pip.py | python3
+pip install scikit-learn
