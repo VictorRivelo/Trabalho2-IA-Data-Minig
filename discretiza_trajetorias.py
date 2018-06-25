9ff068c97c47adb8153c87d0020ea0bbeedf63b1
guilherme <guilherme.milan9@gmail.com>
2018-06-25 03:38:40 -0300

    discretizacao de trajetorias

--- discretiza_trajetorias.py
+++ discretiza_trajetorias.py
@@ -1,46 +1,176 @@
 import pandas as pd
 import numpy as np
+import threading
+import re
 
-def discretiza_trajetorias(numero_de_quadrantes):
-	data = pd.read_csv('DADOS_TREINAMENTO.csv')
-	data = separa_polyline_em_colunas(data)
+def discretiza_trajetorias(numero_de_quadrantes, precisa_separar_lat_long):
+	matriz_lat_long_separadas = pd.DataFrame()
+	if(precisa_separar_lat_long):
+		data = pd.read_csv('DADOS_TREINAMENTO_REDUZIDO.csv')
+		matriz_com_coordenadas_em_colunas = separa_polyline_em_colunas(data)
 
+		matriz_apenas_com_coordenadas = matriz_com_coordenadas_em_colunas.loc[:,'coord0':]
+		#print(matriz_coordenadas)
+		#print(matriz_coordenadas.loc[1:500])
+
+		'''
+		threads = []
+		total_dados = len(matriz_coordenadas.index)
+		qtd_threads = 4
+		intervalos = np.linspace(start=0, stop=total_dados, dtype=int, num=qtd_threads)
+		print(intervalos)
 	
-	latitudes, longitudes = separa_latitude_e_longitude(data['coord0'])
-	print(latitudes)
-	print(longitudes)
+	
+		for i in range(qtd_threads-1):
+		#inicio_intervalo = i*500
+		#fim_intervalo = (i+1)*500-1
+		nova_thread = threading.Thread(target=separa_latitude_e_longitude, args=(matriz_coordenadas.loc[intervalos[i]:intervalos[i+1]-1],i,intervalos[i], intervalos[i+1]-1))
+		threads.append(nova_thread)
+		nova_thread.start()
+		print(i)'''
+	
+		print('vai separar lat e long')
+		matriz_lat_long_separadas = separa_latitude_e_longitude(matriz_apenas_com_coordenadas)
+		matriz_lat_long_separadas.to_csv('dados_sem_colchete.csv')
+	else:
+		matriz_lat_long_separadas = pd.read_csv('dados_sem_colchete.csv')
+		print('dados lidos do arquivo')
+		print(matriz_lat_long_separadas)
 
-	intervalos_latitude = calcula_intervalos_de_discretizacao(latitudes, numero_de_quadrantes)
+	print('lat0')
+	print(matriz_lat_long_separadas['lat0'])
+	print('long0')
+	print(matriz_lat_long_separadas['long0'])
+	print('convertendo tudo em numerico')
+	#print(matriz_lat_long_separadas)
+	#print(latitudes)
+	#print(longitudes)
+
+	intervalos_latitude = calcula_intervalos_de_discretizacao(matriz_lat_long_separadas['lat0'], numero_de_quadrantes)
 	print('intervalos da latitude')
 	print(intervalos_latitude)
 
-	intervalos_longitude = calcula_intervalos_de_discretizacao(longitudes, numero_de_quadrantes)
+	intervalos_longitude = calcula_intervalos_de_discretizacao(matriz_lat_long_separadas['long0'], numero_de_quadrantes)
 	print('intervalos da longitude')
 	print(intervalos_longitude)
+	
+	print('matriz sendo discretizada')
+	print(matriz_lat_long_separadas)
+	i = 0
+	for column in matriz_lat_long_separadas:
+		if(i % 2 == 0):
+			matriz_lat_long_separadas[column] = pd.cut(x=matriz_lat_long_separadas[column], bins=intervalos_latitude)
+			print('coluna discretizada com latitude')
+			print(matriz_lat_long_separadas[column])
+		else:
+			matriz_lat_long_separadas[column] = pd.cut(x=matriz_lat_long_separadas[column], bins=intervalos_longitude)
+			print('coluna discretizada com longitude')
+			print(matriz_lat_long_separadas[column])
+		i += 1
 
-
-	latitudes = pd.cut(x=latitudes, bins=intervalos_latitude)
-	longitudes = pd.cut(x=longitudes, bins=intervalos_longitude)
-	print('coordenadas discretizadas')
-	print(latitudes)
-	print(longitudes)
-
-	data.to_csv('dados_preprocessados.csv')
+	print('matriz discretizada')
+	print(matriz_lat_long_separadas)
+	
+	move_classe_para_ultima_coluna(matriz_lat_long_separadas)
+	
+	matriz_lat_long_separadas.to_csv('dados_preprocessados.csv')
 
 	return 0
 
-def separa_latitude_e_longitude(coluna):
-	coords_separadas = pd.DataFrame(coluna.str.split(',', expand=True))
-	coords_separadas.columns = ['lat0','long0']
+def move_classe_para_ultima_coluna(dados):
+	matriz_nula = pd.isnull(dados)
+	print(matriz_nula)
 
-	coords_separadas['lat0'] = coords_separadas['lat0'].str.strip('[')
-	coords_separadas['lat0'] = pd.to_numeric(coords_separadas['lat0'])
+	i = 0
+	matriz_com_classe = pd.DataFrame()
+	for index, linha in dados.iterrows():
+		print('linha',i)
+		ultimo_indice_valido = linha.last_valid_index()
+		print('ultimo indice valido', ultimo_indice_valido,'valor',linha[ultimo_indice_valido])
+		numero_ultimo_indice_valido = int(re.findall(r'\d+', ultimo_indice_valido)[0])
+		print('numero ultimo_indice_valido', numero_ultimo_indice_valido)
+		linha['classe_lat'] = linha['lat'+str(numero_ultimo_indice_valido)]
+		linha['classe_long'] = linha['long'+str(numero_ultimo_indice_valido)]
+		print('classe lat linha', linha['classe_lat'])
+		print('classe long linha', linha['classe_long'])
+		#print(linha)
+		i += 1
+		matriz_com_classe = matriz_com_classe.append(linha)
 
-	coords_separadas['long0'] = coords_separadas['long0'].str.strip(']')
-	coords_separadas['long0'] = pd.to_numeric(coords_separadas['long0'])
 
-	return coords_separadas['lat0'], coords_separadas['long0']
+	print('final: matriz com classe')
+	print(matriz_com_classe)
+	return matriz_com_classe
 
+#def separa_latitude_e_longitude(dados, num_thread, inicio_intervalo, fim_intervalo):
+def separa_latitude_e_longitude(dados):
+	#print('thread que chamou', num_thread, 'inicio intervalo',inicio_intervalo,'fim intervalo',fim_intervalo,'qtd de dados',len(dados.index))
+	#print(dados)
+	#	print('dado que chegou na funcao de separacao')
+	#print(colunas.columns)
+	#	print('matriz is null')
+	
+	matriz_lat_long_separadas = pd.DataFrame()
+	j = 0
+	for index, linha in dados.iterrows():
+		i = 0
+		qtd_colunas_validas = len(linha.tolist())-linha.isnull().sum()
+		nova_linha = pd.Series()
+		labels = []
+		while (i < qtd_colunas_validas):
+			coluna = linha[i].strip('[').strip(']')
+			labels = ['lat'+str(i),'long'+str(i)]
+			try:
+				nova_linha = nova_linha.append(pd.Series(coluna.split(','), index=labels))
+				#nova_linha[labels[0]] = nova_linha[labels[0]]
+				#nova_linha[labels[1]] = nova_linha[labels[1]]
+			except BaseException:
+				print('EXCEPTION!!!!')
+			i += 1
+
+		#nova_linha = nova_linha.T
+		print('fazendo linha', j)
+		#print('linha resultante')
+		#print(nova_linha)
+		matriz_lat_long_separadas[j] = pd.to_numeric(nova_linha)
+
+		#print('thread',num_thread,'colocou na matriz de lat e long j=', j)
+		j += 1
+	
+	#rint('MATRIZ RESULTANTE DA THREAD ', num_thread)
+	#matriz_lat_long_separadas.index += inicio_intervalo
+	matriz_lat_long_separadas = matriz_lat_long_separadas.transpose()
+	#matriz_lat_long_separadas = matriz_lat_long_separadas.shift(periods=inicio_intervalo, axis=0)
+	print(matriz_lat_long_separadas)
+	
+			#print(matriz_lat_long_separadas)
+	
+		#print(matriz_lat_long_separadas)
+		#break
+	'''for coluna in colunas.columns:
+			if(pd.isna(linha[coluna])):
+				break
+			else:
+				#print('index',index)
+				#print(index,'[',coluna,']=')
+				#print(linha[coluna])
+				lat_label = 'lat'+str(i)
+				long_label = 'long'+str(i)
+
+				coords_separadas = pd.DataFrame(colunas[coluna].str.split(',', expand=True))
+				coords_separadas.columns = [lat_label, long_label]
+
+				coords_separadas[lat_label] = coords_separadas[lat_label].str.strip('[')
+				coords_separadas[lat_label] = pd.to_numeric(coords_separadas[lat_label])
+
+				coords_separadas[long_label] = coords_separadas[long_label].str.strip(']')
+				coords_separadas[long_label] = pd.to_numeric(coords_separadas[long_label])
+
+
+				matriz_lat_long_separadas = matriz_lat_long_separadas.append(coords_separadas, sort=False)
+				i += 1 '''
+
+	return matriz_lat_long_separadas
 
 def calcula_intervalos_de_discretizacao(coluna, numero_de_quadrantes):
 	max_coluna, min_coluna, range_coluna = calcula_range_da_coluna(coluna)
@@ -49,7 +179,9 @@ def calcula_intervalos_de_discretizacao(coluna, numero_de_quadrantes):
 	return intervalos
 
 def calcula_range_da_coluna(column):
+	column = pd.to_numeric(column)
 	statistics = column.describe()
+	print(statistics)
 	column_max = statistics['max']
 	column_min = statistics['min']
 	column_range = np.absolute(column_max-column_min)
@@ -86,4 +218,4 @@ def monta_labels_das_colunas_da_trajetoria(column_num):
 	columns.append('classe')
 	return columns
 
-discretiza_trajetorias(100)
+discretiza_trajetorias(100, True)
