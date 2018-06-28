80605f9cb65015115c2284b96f7aa6ebd6869201
Piuaí <vn.fontenele@gmail.com>
2018-06-27 22:23:14 -0300

    As funções de dia funcionando em modo básico

--- diadasemana.py
+++ diadasemana.py
@@ -1,17 +1,89 @@
-testando edição
+# Feriados de Portugal:
+# 01/01 - Jan
+# 25/04 - Apr
+# 01/05 - May
+# 31/05
+# 10/06 - Jun
+# 15/08 - Aug
+# 05/10 - Oct
+# 01/11 - Nov
+# 01/12 - Dec
+# 08/12
+# 25/12
+# -----
 
-Feriados de portugal:
-01/01
-25/04
-1/5
-31/5
-10/6
-15/8
-5/10
-1/11
-1/12
-8/12
-25/12
+import datetime
+# entra com o timestamp desejado (entrar com o arquivo depois)
+timestamp = input('timestamp: ')
+
+# transforma o timestamp em uma DATA (ANO-MÊS-DIA) -->> DATA = objeto
+d = datetime.date.fromtimestamp(int(timestamp))
+
+# pega o dia da semana correspondente a data acima
+d_weekday = datetime.date.isoweekday(d)
+
+# transforma a data em uma STRING da mesma
+d = d.strftime('%Y-%m-%d')
+
+# quebro a string da data e realoco os valores de ANO, MÊS e DIA para as variáveis correspondentes
+d_ano, d_mes, d_dia = d.split('-')
+
+# print dia da semana correspondente
+if d_weekday == 1:
+    print('Segunda')
+elif d_weekday == 2:
+    print('Terça')
+elif d_weekday == 3:
+    print('Quarta')
+elif d_weekday == 4:
+    print('Quinta')
+elif d_weekday == 5:
+    print('Sexta')
+elif d_weekday == 6:
+    print('Sabado')
+else:
+    print('Domingo')
+
+# checagem de feriados
+if d_dia == '01' and d_mes == '01':
+    feriado = True
+elif d_dia == '24' and d_mes == '04':
+    feriado = True
+elif d_dia == '01' and d_mes == '05':
+    feriado = True
+elif d_dia == '31' and d_mes == '05':
+    feriado = True
+elif d_dia == '10' and d_mes == '06':
+    feriado = True
+elif d_dia == '15' and d_mes == '08':
+    feriado = True
+elif d_dia == '05' and d_mes == '10':
+    feriado = True
+elif d_dia == '01' and d_mes == '11':
+    feriado = True
+elif d_dia == '01' and d_mes == '12':
+    feriado = True
+elif d_dia == '08' and d_mes == '12':
+    feriado = True
+elif d_dia == '25' and d_mes == '12':
+    feriado = True
+else:
+    feriado = False
+
+if feriado == False:
+    print('Não é feriado')
+else:
+    print('É feriado')
+
+
+
+
+
+
+
+
+
+
 
 
 
